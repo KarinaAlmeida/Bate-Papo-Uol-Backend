5b97493d75395a0210d4ea888777e9f3e6838fde
Karina <karinaalmeida523@gmail.com>
2023-01-12 13:41:24 -0300

    rota post e get de participants rodando

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import {MongoClient} from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 
 const server = express();
 dotenv.config();
 server.use(cors());
 server.use(express.json());
+
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 const PORT= process.env.PORT
+let time= dayjs().format("HH:mm:ss");
 
 try{
 await mongoClient.connect()
@@ -19,7 +22,37 @@ console.log("banco conectado")
 }
 const db=mongoClient.db()
 
+server.post("/participants", async (req, res) => {
+    const {name} = req.body;
 
+    try {
+        const userUsado= await db.collection("participants").findOne({name})
+        if (userUsado) return res.status(409).send("Esse usuário já existe!")
+        await db.collection("participants").insertOne({name,lastStatus: Date.now() })
+
+        await db.collection("messages").insertOne({
+            from: name, 
+            to: "Todos", 
+            text: "entra na sala...", 
+            type: "status",
+            time: time})
+
+        return res.sendStatus(201);
+
+    } catch (err) {
+        return res.status(422).send(err.message);
+    }
+});
+
+server.get("/participants", async (req,res) => {
+    try{
+    const listaParticipantes= await db.collection("participants").find({}).toArray();
+    res.send(listaParticipantes);
+    } catch (err) {
+        console.log(err);
+       
+    }
+})
 
 
 
